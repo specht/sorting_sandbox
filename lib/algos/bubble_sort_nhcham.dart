2b1460aa6b6035b42623b7ee96a659bf2595b064
Michael Specht <micha.specht@gmail.com>
2022-09-07 13:47:55 +0200

    yay

---
+++ lib/algos/bubble_sort_nhcham.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:sorting_sandbox/misc.dart';
+
+class BubbleSortNHCham extends SortingAlgorithm {
+  get name => 'Bubble Sort';
+  get color => Colors.green;
+  get author => 'nh_cham';
+
+  void sort(Elements list, Elements scratch) {
+    for (int i = 0; i < list.length; i++) {
+      var x = list[i];
+    }
+  }
+}
